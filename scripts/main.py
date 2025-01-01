f6c1f82857fbd52d1fb35509d004bb807a1031e1
amir <amir@gmail.com>
2025-01-01 05:18:28 +0330

    refactor: improve code with filter_msg and create_logger functions in main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,129 +1,122 @@
-from file_organizer.file_handler import FileHandler
-from config.config import Config
-import argparse
 import logging
+from pathlib import Path
+from typing import Dict, Union, List
+from console_manager.console_manager import ConsoleManager
+from file_handler.file_handler import FileHandler
+from scripts.args import parse_args, get_folders
+from config.config import Config
 
 
-def organize(config_obj: Config) -> None:
-    """Organizes files in the specified folders based on their extensions, moving them to
-    designated subfolders.
-
-    Args:
-        config_obj (Config): A Config object containing the configuration settings.
-
-    Raises:
-        Exception: Any unexpected errors that occur during the file organization process, including 
-                   issues with file access, permission errors, or problems with folder creation.
+def organize_files(
+        configs: Dict[str, Union[Dict[str, str], List[str], bool, str]],
+        console: ConsoleManager,
+        logger: logging.Logger
+) -> None:
     """
-    configs = config_obj.configs
-    handler = FileHandler(folder_path="")
-    extension_to_folder = configs["extension_to_folder"]
+    Organize files in specified folders based on their extensions.
+
+    :param configs: A dictionary containing configuration settings:
+        - folder_paths (list[str, pathlib.Path]): List of folder paths to organize.
+        - extension_to_folder (dict[str, str]): Mapping of file extensions to target folders.
+        - keep_duplicates (bool): Indicates whether to keep duplicate files.
+    :param console: An instance of ConsoleManager for printing messages to the console.
+    :param logger: A logging.Logger instance for logging information and errors.
+
+    :raises TypeError: If a path is not a string or pathlib.Path object.
+    :raises FileExistsError: If a target folder already exists.
+    :raises FileNotFoundError: If a specified file path does not exist.
+    :raises NotADirectoryError: If a target folder does not exist or is not a directory.
+    :raises PermissionError: If a permission error occurs during file movement.
+    """
+    handler = FileHandler(folder_path=Path(""))
+
     folders_to_organize = configs["folder_paths"]
-    keep_dup = configs["keep_duplicates"]
+    extension_to_folder = configs["extension_to_folder"]
+    keep_duplicates = configs["keep_duplicates"]
 
-    for i in range( len(folders_to_organize) ):
-        handler.folder_path = folders_to_organize[i]
+    for folder_path in folders_to_organize:
+        handler.folder_path = Path(folder_path)
 
-        print(f"--- Starting file organization for '{folders_to_organize[i]}'... ---")
-        logger.info(f"Starting file organization for '{folders_to_organize[i]}'")
+        console.print(f"--- Starting file organization for '{folder_path}'... ---")
+        logger.info(f"Starting file organization for '{folder_path}'")
 
         for file in handler.get_files():
-            logger.info(f"Got new file: '{file.name}'")
+            logger.info(f"Processing file: '{file.name}'")
             file_extension = file.suffix
 
-            if file_extension in extension_to_folder:
-                target_folder = extension_to_folder[file_extension]
-                logger.info(f"File has been classfied as - {target_folder.split("/")[0]}")
+            target_folder = extension_to_folder.get(file_extension, "OTHERS")
+            logger.info(f"File classified as - {target_folder.split('/')[0]}")
 
-                try:
-                    handler.create_folder(folder_name=target_folder)
-                    handler.move_file(file=file, folder_name=target_folder, keep_dup=keep_dup)
-                    config_obj.display_status(
-                        status="success",
-                        msg=f"'{file.name}' moved to '{target_folder}' folder"
-                    )
-                except (TypeError, FileExistsError, FileNotFoundError, NotADirectoryError, PermissionError) as e:
-                    config_obj.display_status(
-                        status="failed",
-                        msg=f"[{e.__class__.__name__}] failed to move '{file.name}' to '{target_folder}' folder"
-                    )
-                    logger.warning(f"An error occurred while processing '{file.name}': {e}")
+            try:
+                handler.create_folder(folder_name=target_folder)
+                handler.move_file(file=file, folder_name=target_folder, keep_dup=keep_duplicates)
+                console.print(msg=f"'{file.name}' moved to '{target_folder}' folder", flag="success")
+            except (TypeError, FileExistsError, FileNotFoundError, NotADirectoryError, PermissionError) as e:
+                console.print(
+                    msg=f"[{e.__class__.__name__}] - {e}\nFailed to move '{file.name}' to '{target_folder}' folder",
+                    flag="failed"
+                )
+                logger.warning(f"Error processing '{file.name}': {e}")
 
-            else:
-                logger.info(f"File '{file.name}' has an unsupported extension")
-                try:
-                    handler.create_folder("OTHERS")
-                    handler.move_file(file=file, folder_name="OTHERS", keep_dup=keep_dup)
-                    config_obj.display_status(status="success", msg=f"{file.name} moved to 'OTHERS' folder")
-                except (TypeError, FileExistsError, FileNotFoundError, NotADirectoryError, PermissionError) as e:
-                    config_obj.display_status(
-                        status="failed",
-                        msg=f"[{e.__class__.__name__}] failed to move '{file.name}' to 'OTHERS' folder"
-                    )
-                    logger.warning(f"An error occurred while processing '{file.name}': {e}")
-
-        print(f"--- File organization completed successfully for '{folders_to_organize[i]}' ---")
-        logger.info(f"File organization completed successfully for '{folders_to_organize[i]}'")
+        console.print(f"--- File organization completed successfully for '{folder_path}' ---")
+        logger.info(f"File organization completed successfully for '{folder_path}'")
 
     logger.info("File organization completed successfully for all folders")
 
+
+def filter_messages(console: ConsoleManager) -> None:
+    """
+    Filters messages based on their status level.
+
+    :param console: An instance of ConsoleManager for printing messages to the console.
+    """
+    while True:
+        filter_value = input(
+            "You can filter messages by their status ('success', 'failed', 'all', 'cancel'): "
+        ).lower()
+
+        if filter_value not in ("success", "failed", "all"):
+            break
+
+        console.filter_by_flag(filter_value)
+
+
+def create_logger() -> logging.Logger:
+    """
+    Create a logger instance for logging information and errors.
+
+    :return: A logging.Logger instance
+    """
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        handlers=[logging.FileHandler("file_organizer.log")]
+    )
+
+    return logging.getLogger("main")
+
+
 if __name__ == "__main__":
     try:
-        logging.basicConfig(
-            level=logging.INFO,
-            format="%(asctime)s - %(levelname)s - %(module)s - %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
-            handlers=[logging.FileHandler("file_organizer.log")]
-        )
-        logger = logging.getLogger("main")
+        logger = create_logger()
 
-        # Parse command-line arguments to get configurations
-        parser = argparse.ArgumentParser(description="Organize files into categorized folders.")
+        args = parse_args()
+        folder_paths = get_folders()
 
-        parser.add_argument(
-            "folder_paths",
-            nargs="+",
-            help="Paths to the folders to organize in the format 'path/to/folder'..."
-        )
-
-        parser.add_argument(
-            "--ext",
-            nargs="+",
-            help=(
-                "Specify associated extension for folder in the format '.ext1 folder1' '.ext2 folder2'..."
-            )
-        )
-
-        parser.add_argument(
-            "--dup",
-            type=str.lower,
-            choices=("true", "false"),
-            help=(
-                "If 'true', renames duplicate files to avoid overwriting, and"
-                "if 'false', overwrites existing files with the same name"
-            )
-        )
-
-        parser.add_argument(
-            "--status",
-            type=str.lower,
-            choices=("all", "success", "failed"),
-            help=(
-                "Determines which messages to print in the console based on their status. "
-                "Choose from 'all', 'success', or 'failed'."
-            )
-        )
-
-        args = parser.parse_args()
         config = Config()
         config.set_configs(
+            folder_paths=folder_paths,
             extension_to_folder=args.ext,
-            folder_paths=args.folder_paths,
             keep_duplicates=args.dup,
             status_level=args.status
         )
+        configs = config.configs
 
-        organize(config_obj=config)
+        console = ConsoleManager(output_level=configs["status_level"])
+
+        organize_files(configs, console, logger)
+        filter_messages(console)
 
     except Exception as e:
         logger.exception(f"An unexpected error occurred: {e}")
