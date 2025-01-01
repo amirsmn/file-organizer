8ccbcd785a660cb55b7653f10459b096581eaf0b
amir <amir@gmail.com>
2025-01-01 05:20:43 +0330

    refactor: improve configuration setup code for better clarity and efficiency

--- config/config.py
+++ config/config.py
@@ -1,50 +1,46 @@
-from typing import Dict, Union, List
 import logging
-import colorama
 import json
+from typing import Dict, Union, List
+
+
+logger = logging.getLogger(__name__)
 
 
 class Config:
-    """Handles loading, setting configurations from/to a JSON file.
-
-    This class provides functionality to load configuration from a JSON file, set new
-    configurations, and display the status of certain operations based on a configured
-    level of verbosity.
-
-    Attributes:
-            config_file (str): The path to the JSON configuration file.
-            configs (Dict): Loaded configurations from the JSON file.
+    """
+    Handles loading and setting configurations from/to a JSON file.
     """
 
     def __init__(self, config_file: str = "config/config.json") -> None:
-        """Initializes the Config class with the given configuration file.
+        """
+        Initializes the Config class with the given configuration file.
 
-        Args:
-            config_file (str, optional): The path to the configuration file. Defaults to
-                                         "config/config.json".
+        :param config_file: The path to the configuration file. Defaults to "config/config.json".
+        :type config_file: str, optional
         """
         self.config_file = config_file
         self.configs = self.get_configs()
 
     @property
     def config_file(self) -> str:
-        """Returns the path to the configuration file.
+        """
+        Returns the path to the configuration file.
 
-        Returns:
-            str: The path to the configuration file.
+        :returns: The path to the configuration file.
+        :rtype: str
         """
         return self._config_file
 
     @config_file.setter
     def config_file(self, file: str) -> None:
-        """Sets the configuration file path after validating the file type.
+        """
+        Sets the configuration file path after validating the file type.
 
-        Args:
-            file (str): The path to the configuration file.
+        :param file: The path to the configuration file.
+        :type file: str
 
-        Raises:
-            TypeError: If `file` is not a string.
-            ValueError: If the file is not a JSON file.
+        :raises TypeError: If `file` is not a string.
+        :raises ValueError: If the file is not a JSON file.
         """
         if not isinstance(file, str):
             raise TypeError("file should be an `str` object")
@@ -52,56 +48,45 @@ class Config:
         if file.endswith(".json"):
             self._config_file = file
         else:
-            logging.error(f"Config file '{file}' should be a JSON file")
-            raise ValueError(f"Invalide file type: {file}. Only '.json' files are allowed")
+            logger.error(f"Config file '{file}' should be a JSON file")
+            raise ValueError(f"Invalid file type: {file}. Only '.json' files are allowed")
 
     def get_configs(self) -> Dict[str, Union[Dict[str, str], List[str], bool, str]]:
-        """Loads the configuration data from the JSON file.
+        """
+        Loads the configuration data from the JSON file.
 
         This method reads the configuration file, verifies required fields, and validates
         their values.
 
-        Returns:
-            Dict[str, Union[Dict[str, str], List[str], bool, str]]: The loaded configuration data.
+        :returns: The loaded configuration data.
+        :rtype: dict
 
-        Raises:
-            FileNotFoundError: If the configuration file does not exist.
-            KeyError: If any required fields are missing from the configuration file.
-            ValueError: If any validation errors occur (e.g., empty folder paths or too many folders).
+        :raises FileNotFoundError: If the configuration file does not exist.
+        :raises KeyError: If any required fields are missing from the configuration file.
+        :raises ValueError: If any validation errors occur (e.g., empty folder paths or too many folders).
         """
         try:
             with open(self.config_file) as config:
                 config_data = json.load(config)
 
-            if "extension_to_folder" not in config_data:
-                raise KeyError(f"Missing 'extension_to_folder' in '{self.config_file}'")
+            required_fields = {"extension_to_folder", "folder_paths", "keep_duplicates", "status_level"}
 
-            if "folder_paths" not in config_data:
-                raise KeyError(f"Missing 'folder_paths' in '{self.config_file}'")
-            elif not (folders_count := len(config_data["folder_paths"])):
+            missing_fields = required_fields - config_data.keys()
+            if missing_fields:
+                raise KeyError(f"Missing '{missing_fields}' in '{self.config_file}'")
+
+            if not len(config_data["folder_paths"]):
                 raise ValueError(f"'folder_paths' in {self.config_file} cannot be empty")
-            elif folders_count > 20:
-                raise ValueError(
-                        f"folder_paths in {self.config_file} is too large. "
-                        f"Current folders number is {folders_count}"
-                )
-
-            if "keep_duplicates" not in config_data:
-                raise KeyError(f"Missing 'keep_duplicates' in '{self.config_file}'")
-
-            if "status_level" not in config_data:
-                raise KeyError(f"Missing 'status_level' in '{self.config_file}'")
+            elif len(config_data["folder_paths"]) > 20:
+                raise ValueError(f"'folder_paths' in {self.config_file} is too large. It should be less than 20")
 
             return config_data
 
         except FileNotFoundError:
-            logging.error(f"Configuration file '{self.config_file}' not found.")
+            logger.error(f"Configuration file '{self.config_file}' not found.")
             raise
-        except KeyError as e:
-            logging.error(e)
-            raise
-        except ValueError as e:
-            logging.error(e)
+        except (KeyError, ValueError) as e:
+            logger.error(e)
             raise
 
     def set_configs(
@@ -112,20 +97,23 @@ class Config:
             keep_duplicates: str = None,
             status_level: str = None
     ) -> None:
-        """Updates the configuration file with new values.
+        """
+        Updates the configuration file with new values.
 
         This method allows you to update specific configurations in the JSON file, such as adding
         new extensions and folders, changing the duplicate file behavior, or updating the status level.
 
-        Args:
-            extension_to_folder (List[str], optional): A list of extension-folder mappings to update.
-            folder_paths (List[str], optional): A list of folder paths to update.
-            keep_duplicates (str, optional): A string indicating whether to keep duplicates ("true" or "false").
-            status_level (str, optional): The level of status to display ("all", "success", or "failed").
+        :param extension_to_folder: A list of extension-folder mappings to update.
+        :type extension_to_folder: list, optional
+        :param folder_paths: A list of folder paths to update.
+        :type folder_paths: list, optional
+        :param keep_duplicates: A string indicating whether to keep duplicates ("true" or "false").
+        :type keep_duplicates: str, optional
+        :param status_level: The level of status to display ("all", "success", or "failed").
+        :type status_level: str, optional
 
-        Raises:
-            TypeError: If any argument is not of the expected type.
-            FileNotFoundError: If the configuration file does not exist.
+        :raises TypeError: If any argument is not of the expected type.
+        :raises FileNotFoundError: If the configuration file does not exist.
         """
         if not (
             (isinstance(extension_to_folder, list) or extension_to_folder is None) and
@@ -143,7 +131,7 @@ class Config:
                 for data in extension_to_folder:
                     if data := data.strip():
                         data_parts = data.split(" ", 1)
-                        if len(data_parts) == 2 and "." in data_parts[0]:
+                        if len(data_parts) == 2 and data_parts[0].startswith("."):
                             config_data["extension_to_folder"].update({data_parts[0]: data_parts[1].strip()})
 
             if (folder_paths is not None) and (0 < len(folder_paths) < 20):
@@ -159,36 +147,11 @@ class Config:
 
             with open(self.config_file, "w") as config:
                 json.dump(config_data, config, indent=4)
-            logging.info("New configurations have been set successfully")
+            logger.info("New configurations have been set successfully")
 
         except FileNotFoundError as e:
-            logging.error(f"Configuration file '{self.config_file}' not found.")
+            logger.error(f"Configuration file '{self.config_file}' not found.")
             raise
         except TypeError as e:
-            logging.error(e)
+            logger.error(e)
             raise
-
-    def display_status(self, status: str, msg: str) -> None:
-        """Displays a status message to the console, formatted with color based on the status.
-
-        Args:
-            status (str): The status of the operation ("success" or "failed").
-            msg (str): The message to display.
-
-        If the `status_level` in the configuration is set to a value that does not match the provided
-        `status`, the message is not displayed.
-        """
-        if self.configs["status_level"] != "all" and self.configs["status_level"] != status:
-            return
-
-        status_symbols = {
-            "success": f"{colorama.Fore.GREEN}●{colorama.Fore.RESET}",
-            "failed": f"{colorama.Fore.RED}●{colorama.Fore.RESET}",
-        }
-
-        if status not in status_symbols:
-            logging.warning(f"Invalid status: '{status}'. Valid options are 'success' and 'failed'")
-            return
-
-        print(f"{status_symbols[status]} {msg}")
-
